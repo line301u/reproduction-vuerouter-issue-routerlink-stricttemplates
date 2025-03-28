56555f3e17cd465c5ce08b06f13b4b7b363e1fe8
line301u <61137927+line301u@users.noreply.github.com>
2025-03-28 12:58:48 +0100

    create reproduction of routerlink bug with strictTemplates

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <main>
+    <RouterLink to="/" @focus="console.log('focus')">test</RouterLink>
+  </main>
+</template>
